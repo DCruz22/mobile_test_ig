f3fa9757e9a5ed77011f307468b6da99ab099488
dcruz22 <darielnaturalist@gmail.com>
2021-09-15 16:34:16 -0400

    Starting with the ui

--- app/src/main/java/com/example/koombea_ig/ui/MainActivity.kt
+++ app/src/main/java/com/example/koombea_ig/ui/MainActivity.kt
@@ -3,8 +3,14 @@ package com.example.koombea_ig.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.koombea_ig.R
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel: MainViewModel by viewModel()
+
+    private lateinit var viewBinding: MainActivity
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
