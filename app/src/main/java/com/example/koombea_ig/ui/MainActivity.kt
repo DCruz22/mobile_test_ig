2ea4401737fed641985bd3be676b00fbcb34e447
dcruz22 <darielnaturalist@gmail.com>
2021-09-15 09:38:40 -0400

    initial commit

---
+++ app/src/main/java/com/example/koombea_ig/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.koombea_ig.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.koombea_ig.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
